8d4c6741f33b81a56dcebc991ce63eeb23ba1627
AitorMelero <meleropiconaitor@gmail.com>
2024-03-26 10:01:23 +0100

    feat: Add animation in 404 page

--- public/js/animate.js
+++ public/js/animate.js
@@ -1,12 +1,12 @@
-function animate() {
+function animate () {
   const animateElements = document.querySelectorAll('.animate')
 
   animateElements.forEach((element, index) => {
     setTimeout(() => {
       element.classList.add('show')
     }, index * 150)
-  });
+  })
 }
 
-document.addEventListener("DOMContentLoaded", animate)
-document.addEventListener("astro:after-swap", animate)
+document.addEventListener('DOMContentLoaded', animate)
+document.addEventListener('astro:after-swap', animate)
